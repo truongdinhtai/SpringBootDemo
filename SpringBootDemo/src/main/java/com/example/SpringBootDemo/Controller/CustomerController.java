3614ec9be448a736cb252e98a85ad6dd8d61bd6a
taitruong2320 <52031560+taitruong2320@users.noreply.github.com>
2022-06-29 17:35:22 +0700

    update code stage 5 write unit test

--- SpringBootDemo/src/main/java/com/example/SpringBootDemo/Controller/CustomerController.java
+++ SpringBootDemo/src/main/java/com/example/SpringBootDemo/Controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.example.SpringBootDemo.Controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringBootDemo.DTO.CustomerDTO;
 import com.example.SpringBootDemo.Entity.Customer;
 import com.example.SpringBootDemo.Service.CustomerService;
+import com.example.SpringBootDemo.Utils.AppUtils;
 import com.example.SpringBootDemo.Utils.JsonUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    private static Logger logger = LoggerFactory.getLogger(CustomerController.class.getName());
 
     @Autowired
     private CustomerService customerService;
@@ -29,8 +35,18 @@ public class CustomerController {
      */
     @GetMapping
     public ResponseEntity<List<Customer>> getAllCustomers() {
-        List<Customer> customers = customerService.getCustomers();
-        return new ResponseEntity<>(customers, HttpStatus.OK);
+        logger.info("in!!");
+        try {
+            logger.info("List Customer!!");
+            List<Customer> customers = customerService.getAllCustomers();
+            return new ResponseEntity<>(customers, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("error");
+            e.printStackTrace();
+            logger.error("Exception -" + AppUtils.getLogSupport(e));
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        logger.info("out!!");
     }
 
     /**
@@ -40,8 +56,18 @@ public class CustomerController {
      */
     @PostMapping("/inserCustomer")
     public ResponseEntity<Object> saveCusotmer(@RequestBody String json) {
-        CustomerDTO customerDTO = new CustomerDTO();
-        customerDTO = (CustomerDTO) JsonUtils.convertToObject(json, new TypeReference<CustomerDTO>() {});
-        return new ResponseEntity<>(customerService.insert(customerDTO), HttpStatus.CREATED);
+
+        logger.info("in!!");
+        try {
+            CustomerDTO customerDTO = new CustomerDTO();
+            customerDTO = (CustomerDTO) JsonUtils.convertToObject(json, new TypeReference<CustomerDTO>() {});
+            return new ResponseEntity<>(customerService.insert(customerDTO), HttpStatus.CREATED);
+        } catch (Exception e) {
+            logger.error("error");
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        logger.info("out!!");
     }
+
+    
 }
---
+++ SpringBootDemo/src/test/java/com/example/SpringBootDemo/Controller/CustomerControllerTest.java
@@ -0,0 +1,88 @@
+package com.example.SpringBootDemo.Controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.example.SpringBootDemo.DTO.CustomerDTO;
+import com.example.SpringBootDemo.Entity.Customer;
+import com.example.SpringBootDemo.Service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+	@Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /*
+     *  Unit test insert customer
+     */
+    @Test
+    public void testInserCustomer() throws Exception {
+        Customer customer = Customer.builder()
+                .customerName("qưe")
+                .customerEmail("zsd")
+                .phone("qưeeee")
+                .build();
+        given(customerService.insert((CustomerDTO) any(Customer.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        ResultActions response = mockMvc.perform(post("/api/v1/customer/inserCustomer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)));
+
+        response.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.customerName",
+                        is(customer.getCustomerName())))
+                .andExpect(jsonPath("$.CustomerEmail",
+                        is(customer.getCustomerEmail())))
+                .andExpect(jsonPath("$.phone",
+                        is(customer.getPhone())));
+    }
+
+    /*
+     *  Unit test get all customer
+     */
+    @Test
+    public void testAllCustomer() throws Exception {
+        List<Customer> listCustomer = new ArrayList<>();
+        listCustomer.add(Customer.builder().customerName("CaptainMarvel").customerEmail("cap@gmail.com").phone("0909192").build());
+        listCustomer.add(Customer.builder().customerName("TonyStark").customerEmail("tony@gmail.com").phone("0909192").build());
+        given(customerService.getAllCustomers()).willReturn(listCustomer);
+
+        ResultActions response = mockMvc.perform(get("/api/v1/customer"));
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()",
+                        is(listCustomer.size())));
+    }
+}
