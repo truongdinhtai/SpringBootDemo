c67936b3c209379158b188d62443f0005dff1310
taitruong2320 <52031560+taitruong2320@users.noreply.github.com>
2022-06-29 17:40:00 +0700

    update code stage 5 use querydls

--- SpringBootDemo/src/main/java/com/example/SpringBootDemo/Repository/CustomerRepository.java
+++ SpringBootDemo/src/main/java/com/example/SpringBootDemo/Repository/CustomerRepository.java
@@ -5,7 +5,15 @@ import org.springframework.stereotype.Repository;
 import com.example.SpringBootDemo.DTO.CustomerDTO;
 import com.example.SpringBootDemo.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
+    @Modifying
+    @Query(value = "insert into Customer (id,customer_Name,customer_Email, phone) "
+            + "VALUES(:id,:customerName,:customerEmail,:phone)", nativeQuery = true)
+    public void insertCustomerUsingQueryAnnotationn(@Param("id") Long id, @Param("customerName") String customerName,
+            @Param("customerEmail") String customerEmail, @Param("phone") String phone);
 }
